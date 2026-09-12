3c5aa8bfafa4b4f73310fc9aa71ff6a4c1d9c372
exelix <13405476+exelix11@users.noreply.github.com>
2026-09-12 17:56:40 +0200

    Use correct path in the sd files build script

--- mono-nx/aot/collect_sd_release.sh
+++ mono-nx/aot/collect_sd_release.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [ ! -e sd_card/osu_stream/icudt77l.dat ]; then
+if [ ! -e sd_card/osu-stream/icudt77l.dat ]; then
     echo copying full icu data file
-    cp $ICU_NX_INSTALL_DIR/share/icu/77.1/icudt77l.dat sd_card/osu_stream/
+    cp $ICU_NX_INSTALL_DIR/share/icu/77.1/icudt77l.dat sd_card/osu-stream/
 fi
 
-cp -r ../../osu\!stream/bin/Debug/net9.0/Beatmaps/ sd_card/osu_stream/Beatmaps/
-cp -r ../../osu\!stream/bin/Debug/net9.0/Skins/ sd_card/osu_stream/Skins/
-cp -r ../../osu\!stream/bin/Debug/net9.0/Localisation/ sd_card/osu_stream/Localisation/
+cp -r ../../osu\!stream/bin/Debug/net9.0/Beatmaps/ sd_card/osu-stream/Beatmaps/
+cp -r ../../osu\!stream/bin/Debug/net9.0/Skins/ sd_card/osu-stream/Skins/
+cp -r ../../osu\!stream/bin/Debug/net9.0/Localisation/ sd_card/osu-stream/Localisation/
 
 if [ ! -d sd_card/switch ]; then
     mkdir -p sd_card/switch
